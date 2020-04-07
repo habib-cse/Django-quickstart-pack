7008aabbe43c1a33fab5dc0ff29d17c4c9c83c2a
habib-cse <habib41juwel@gmail.com>
2020-04-07 17:37:59 +0600

    all file added

---
+++ ecom/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+import debug_toolbar
+from django.conf.urls.static import static 
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
+    path("", include('core.urls', namespace="core"))
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
